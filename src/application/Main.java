a9a18bdf1533824f7f6ee31fd14e3312a8fa15d0
Joao Victor Baptista <joaovictordacostabaptista@hotmail.com>
2023-09-23 20:38:17 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,14 +12,11 @@ public class Main {
     {
         Department obj = new Department(1, "Books");
 
-        Department obj2 = new Department(2,"Eletronics");
-
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
-        Seller seller2 = new Seller(23, "Guilherme FS","guilhermerj@gmail.com",new Date(), 2000.0, obj2);
+        SellerDao sellerDao =  DaoFactory.createSellerDao();
 
         System.out.println(seller);
-        System.out.println();
-        System.out.println(seller2);
+
     }
 }
