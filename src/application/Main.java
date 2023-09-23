a7039cf86f819d9d5a65947974484a5c616eec3c
Joao Victor Baptista <joaovictordacostabaptista@hotmail.com>
2023-09-23 20:05:30 -0300

    Department and SellerDao

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,14 @@ public class Main {
     {
         Department obj = new Department(1, "Books");
 
+        Department obj2 = new Department(2,"Eletronics");
+
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        Seller seller2 = new Seller(23, "Guilherme FS","guilhermerj@gmail.com",new Date(), 2000.0, obj2);
+
         System.out.println(seller);
+        System.out.println();
+        System.out.println(seller2);
     }
 }
