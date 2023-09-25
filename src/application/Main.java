a3297fa17ccd6465e7b66bc5ff2cdb6bee95069b
Joao Victor Baptista <joaovictordacostabaptista@hotmail.com>
2023-09-25 19:42:49 -0300

    Reusing instantiation

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,8 +12,8 @@ public class Main {
     {
         SellerDao sellerDao =  DaoFactory.createSellerDao();
 
+        System.out.println("===== Teste 1: seller findById =====");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
 
     }
